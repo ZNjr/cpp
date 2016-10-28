7543465208ef75cea56e1919c534c2e08f5901ad
ZNjr <znajer@gmail.com>
2016-10-28 21:38:07 +0200

    Update dzialania_na_macierzach.cpp

--- dzialania_na_macierzach.cpp
+++ dzialania_na_macierzach.cpp
@@ -2,9 +2,7 @@
 #include <cmath>
  
  using namespace std;
- 
- 
- 
+
  int** dodawanie(int **matrix_1 ,int **matrix_2){
 	 int** wartosci_po_dodawaniu = new int * [sizeof(matrix_1)];
  	  
@@ -19,10 +17,10 @@
  }  
  void wypisz(int** matrix){
  	for(int i=0;i<sizeof(matrix);i++){
- 		for(int j=0;j<sizeof(matrix);j++){
-				cout.width(8);
-				cout << matrix[i][j];
-	 }
+ 	    for(int j=0;j<sizeof(matrix);j++){
+		   cout.width(8);
+		   cout << matrix[i][j];
+	      }
  	cout << endl;
    }
 }
@@ -30,16 +28,13 @@
 //int** mnozenie(){	
 //}
  int main(){
- 	
- 	
  	int** matrix = new int*[8];
- 	
- 	for(int i=0;i<sizeof(matrix);i++){
+ 	    for(int i=0;i<sizeof(matrix);i++){
  		matrix[i] = new int[sizeof(matrix)];
-	 	for(int j=0;j<sizeof(matrix);j++){
+	 	    for(int j=0;j<sizeof(matrix);j++){
  			matrix[i][j] = pow(j+1,i);
-		 }
-	 }
+		      }
+	 	}
 	
  int** matrix_po_dodawaniu = dodawanie(matrix,matrix);
  wypisz(matrix);
